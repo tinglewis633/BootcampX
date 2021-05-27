f4066f8b6ed59f213900873781ba081748b2d3e2
Lewis <tinglewis633@gmail.com>
2021-05-27 02:54:49 +0000

    para query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,21 +6,15 @@ const pool = new Pool({
   host: "localhost",
   database: "bootcampx",
 });
-
-const date = process.argv.slice(2)[0];
-const limit = process.argv.slice(2)[1];
-
-pool
-  .query(
-    `
-    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts
-    ON cohorts.id = cohort_id
-    LIMIT ${limit};
-    `
-  )
-  .then((res) => {
-    console.log(res.rows);
-  })
-  .catch((err) => console.error("query error", err.stack));
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+pool.query(queryString, values);
